e14a223b2f70f07d161b2906e69c2e592d5193a2
samiksha1681 <samiksha1681@gmail.com>
2016-08-17 22:48:53 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package com.verizon.employee.beans;
+
+public class Employee {
+private String empname;
+private int empcode;
+private int salary;
+public String getEmpname() {
+	return empname;
+}
+public void setEmpname(String empname) {
+	this.empname = empname;
+}
+public int getEmpcode() {
+	return empcode;
+}
+public void setEmpcode(int empcode) {
+	this.empcode = empcode;
+}
+public int getSalary() {
+	return salary;
+}
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+
+}
